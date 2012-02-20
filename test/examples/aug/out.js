b8353056903b0324c0440d3d48eb14608bc4fa5d
Greg Allen <me@jga.me>
2012-02-19 16:49:12 -0800

    got working with amd, added --verify option

---
+++ test/examples/aug/out.js
@@ -0,0 +1,27 @@
+var aug = (function(exports, module) {
+var cs0 = (function(exports, module) {
+!function (name, definition) {
+  if (typeof module != 'undefined' && exports) exports = definition();
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+  else this[name] = definition();
+}('aug', function() {
+
+var aug = function __aug() {
+  var args = Array.prototype.slice.call(arguments);
+  var org = args.shift();
+  for (var i = 0, c = args.length; i < c; i++) {
+    var prop = args[i];
+    for (var name in prop) {
+      org[name] = prop[name];
+    }
+  }
+  return org;
+};
+
+return aug;
+});
+
+return exports;
+})({}, true);
+return cs0;
+})({}, true);
