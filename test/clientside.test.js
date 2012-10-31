8a49134df599033b7c5aa41ef992ca2dad5350d9
Greg Allen <me@jga.me>
2012-10-31 08:41:35 -0700

    removed lib.main

--- test/clientside.test.js
+++ test/clientside.test.js
@@ -17,7 +17,6 @@ describe('clientside', function() {
     var source;
 
     beforeEach(function(done) {
-
       clientside(fixtureDir + 'c.js', 'name', function(err, results) {
 
         var sandbox = {
@@ -36,22 +35,25 @@ describe('clientside', function() {
         //console.log(out);
         done();
       });
-
     });
+
     it('should add comment to top with clientside version', function() {
       assert.ok(source.match(/built with clientside/));
       assert.ok(source.match('built with clientside '+version));
     });
+
     it('should create __cs object', function() {
       assert.equal(typeof out.__cs, 'object');
       assert.equal(typeof out.__cs.map, 'object');
       assert.equal(typeof out.__cs.libs, 'object');
       assert.equal(typeof out.__cs.r, 'function');
     });
+
     it('should have found two unique requires', function() {
       assert.equal(typeof out.__cs.map['./a'], 'string');
       assert.equal(typeof out.__cs.map['./b'], 'string');
     });
+
     it('should have loaded 3 libs', function() {
 
       var count = 0;
@@ -60,31 +62,28 @@ describe('clientside', function() {
       }
       assert.equal(count, 3);
     });
-    it('should have set __cs.libs.main to main file', function() {
-      assert.equal(typeof out.__cs.libs.main, 'object');
-      assert.equal(out.__cs.libs.main.a, 'a');
-      assert.equal(out.__cs.libs.main.b, 'ab');
-    });
 
     it('require(./a) should return a function', function() {
       //__cs.r == require inside module
       assert.equal(typeof out.__cs.r('./a'), 'function');
       assert.equal(out.__cs.r('./a')(), 'a');
     });
+
     it('require(./b) should return a function', function() {
       assert.equal(typeof out.__cs.r('./b'), 'function');
       assert.equal(out.__cs.r('./b')(), 'ab');
     });
+    
     it('name should be results from c.js', function() {
       assert.equal(typeof out.name, 'object');
       assert.equal(out.name.a, 'a');
       assert.equal(out.name.b, 'ab');
     });
+
     it('should expose global require function', function() {
       assert.equal(typeof out.window.require, 'function');
       assert.equal(typeof out.window.require('./b'), 'function');
       assert.equal(out.window.require('./b')(), 'ab');
-      
     });
 
     
